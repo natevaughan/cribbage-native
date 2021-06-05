cfbe98e02f0780629f8fa59e0b80d2b90f052023
Nate Vaughan <nate.vaughan@protonmail.com>
2021-06-05 13:46:46 -0500

    improve legibility

--- components/PegHole.tsx
+++ components/PegHole.tsx
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
         width: 6,
         height: 6,
         borderRadius: 3,
-        backgroundColor:'#666666',
+        backgroundColor:'#ccc',
         marginRight: 3
     }
 });
